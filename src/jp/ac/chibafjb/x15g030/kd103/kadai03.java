3c6dffe109f53c940141bad429e80f254108c762
unknown <x15g030@PC504N009.chiba-fjb.ac.jp>
2015-11-02 14:47:27 +0900

    課題03

---
+++ src/jp/ac/chibafjb/x15g030/kd103/kadai03.java
@@ -0,0 +1,20 @@
+package jp.ac.chibafjb.x15g030.kd103;
+
+public class kadai03 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.print("1 船橋 太郎");
+		System.out.print("2 船橋 太郎");
+		System.out.print("3 船橋 太郎");
+		System.out.print("4 船橋 太郎");
+		System.out.print("5 船橋 太郎");
+		System.out.print("6 船橋 太郎");
+		System.out.print("7 船橋 太郎");
+		System.out.print("8 船橋 太郎");
+		System.out.print("9 船橋 太郎");
+		System.out.print("10 船橋 太郎");
+
+	}
+
+}
