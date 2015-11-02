701600c6ff646d42fc5b0f5a0c0880faac9d7170
unknown <x15g030@PC504N009.chiba-fjb.ac.jp>
2015-11-02 14:49:49 +0900

    課題04

---
+++ src/jp/ac/chibafjb/x15g030/kd104/kadai04.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g030.kd104;
+
+public class kadai04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("あいうえお あいうえお あいうえお");
+		System.out.println("あいうえお あいうえお あいうえお");
+		System.out.println("あいうえお あいうえお あいうえお");
+	}
+
+}
