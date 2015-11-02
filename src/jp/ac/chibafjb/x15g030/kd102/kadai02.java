fe6d9e301e8eeab22fdc88bba6850f021935e027
unknown <x15g030@PC504N009.chiba-fjb.ac.jp>
2015-11-02 14:40:27 +0900

    課題02

---
+++ src/jp/ac/chibafjb/x15g030/kd102/kadai02.java
@@ -0,0 +1,21 @@
+
+package jp.ac.chibafjb.x15g030.kd102;
+
+public class kadai02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("1 船橋 太郎");
+		System.out.println("2 船橋 太郎");
+		System.out.println("3 船橋 太郎");
+		System.out.println("4 船橋 太郎");
+		System.out.println("5 船橋 太郎");
+		System.out.println("6 船橋 太郎");
+		System.out.println("7 船橋 太郎");
+		System.out.println("8 船橋 太郎");
+		System.out.println("9 船橋 太郎");
+		System.out.println("10 船橋 太郎");
+
+	}
+
+}
