f46ae72304c3f9eb31e21e3b134c4ffabda7e7c6
unknown <x15g030@PC504N009.chiba-fjb.ac.jp>
2015-11-02 14:54:47 +0900

    課題05

---
+++ src/jp/ac/chibafjb/x15g030/kd105/kadai05.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g030.kd105;
+
+public class kadai05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("氏名 : 船橋 太郎");
+		System.out.println("趣味 : ゲーム");
+		System.out.println("好きな飲み物 : MAXコーヒー");
+		
+	}
+
+}
