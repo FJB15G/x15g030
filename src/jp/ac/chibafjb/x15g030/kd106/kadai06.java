c59630074acee56014dd41c1cd6d9251476e91ce
unknown <x15g030@PC504N009.chiba-fjb.ac.jp>
2015-11-02 15:03:39 +0900

    課題06

---
+++ src/jp/ac/chibafjb/x15g030/kd106/kadai06.java
@@ -0,0 +1,30 @@
+package jp.ac.chibafjb.x15g030.kd106;
+
+public class kadai06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("  a@@@a a@@@a");
+		System.out.println("  @@@@@@e@@@@@@");
+		System.out.println(" (@@@@@@@@@@@@@)");
+		System.out.println("  @@@@@@@@@@@@@");
+		System.out.println("   @@@@@@@@@@@");
+		System.out.println("    ~@@@@@@@~");
+		System.out.println("      ~@@@~");
+		System.out.println("       ~@~　　　ハート！");
+		
+		
+		
+		System.out.println("　        ＋―――――――――――＋");
+		System.out.println("　　　　　│　　／￣￣￣＼　　＿　│");
+		System.out.println("　《《　　│　／o）《《《 ＼／ ／ │");
+		System.out.println("　  》》　│ │――――――  く　 │");
+		System.out.println("　┌―┐　│　＼o）《《《 ／＼ ＼ │");
+		System.out.println("　│　│　│　　＼＿＿＿／ △ ￣　│");
+		System.out.println("　└―┘　＋―――――――――――＋");
+		System.out.println("              　あじのひらき");
+		
+		
+	}
+
+}
