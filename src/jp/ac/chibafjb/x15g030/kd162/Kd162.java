a3759fe8ce371cb6574f9669e5a2a65912c3c890
unknown <x15g030@PC504N009.chiba-fjb.ac.jp>
2015-12-07 15:11:21 +0900

    課題10

---
+++ src/jp/ac/chibafjb/x15g030/kd162/Kd162.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g030.kd162;
+
+public class Kd162 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int [] sum;
+		sum = new int [5];
+		sum[0]=33;
+		sum[1]=18;
+		sum[2]=102;
+		sum[3]=47;
+		sum[4]=5;
+		System.out.println(sum[0]+sum[1]+sum[2]+sum[3]+sum[4]);
+	}
+
+}
