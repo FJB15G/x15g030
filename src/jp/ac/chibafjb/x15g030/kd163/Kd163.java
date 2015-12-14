b6b655c7c30df2ea1ecf5c439d65b913505a496e
unknown <x15g030@PC504N009.chiba-fjb.ac.jp>
2015-12-14 14:59:50 +0900

    課題11

--- src/jp/ac/chibafjb/x15g030/kd163/Kd163.java
+++ src/jp/ac/chibafjb/x15g030/kd163/Kd163.java
@@ -1,18 +1,18 @@
-package jp.ac.chibafjb.x15g030.kd162;
+package jp.ac.chibafjb.x15g030.kd163;
 
-public class Kd162 {
+public class Kd163 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int sum;
 		sum =0;
 		int[] x= {33,18,102,47,5};
-		for (int a=0; a<5;a++){
+		int n=x.length;
+		
+		for (int a=0; a<n;a++){
 			sum=sum+x[a];
 		}
 			System.out.println("合計　=　"+sum);
-		
-	
-	
-}
+	}
+
 }
