755470bf3eb9ba364b54aea33df469430d3faa5f
unknown <x15g030@PC504N009.chiba-fjb.ac.jp>
2016-01-18 14:24:31 +0900

    課題12

---
+++ src/jp/ac/chibafjb/x15g030/kd164/Kd164.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g030.kd164;
+
+public class Kd164 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	
+		int[] a ={5,10,15};
+			
+			int []b;
+			b = new int [a.length];
+			int m=a.length;
+			for (int i=0;i<m;i++){
+				b[i]=a[i];
+			System.out.println("b["+i+"]="+b[i]);
+			}
+	}
+}
---
+++ src/jp/ac/chibafjb/x15g030/kd165/Kd165.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g030.kd165;
+
+public class Kd165 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int[] a ={15,10,5};
+		
+		int []b;
+		b = new int [a.length];
+		int m=a.length;
+		for (int i=0;i<m;i++){
+			b[i]=a[i];
+		System.out.println("b["+i+"]="+b[i]);
+		}
+}
+}
